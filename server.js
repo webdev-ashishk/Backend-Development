49b7072f758d4a5345f0a2ef60ea2890fa9e020b
webdev-ashishk <webdev.ashishk@gmail.com>
2023-10-29 22:10:09 +0530

    Day#2- 56min;10second

--- server.js
+++ server.js
@@ -18,22 +18,15 @@ app.use(function (req, res, next) {
 app.get("/", function (req, res) {
   res.send("Welcome to Backend development by YT by Indian Creater !");
 });
-app.get("/profile", function (req, res) {
-  res.send("This is great profile build by hard-work");
-});
+
 //Dynamic routes
-app.get("/profile/:usename", function (req, res) {
-  res.send(`hello from profile ${usename}`);
+app.get("/profile/:username", function (req, res) {
+  res.send(`hello from profile ${req.params.username}`);
 });
-// app.get("/profile/anju", function (req, res) {
-//   res.send("This is great profile build by hard-work");
-// });
-// app.get("/profile/deepika", function (req, res) {
-//   res.send("This is great profile build by hard-work");
-// });
-// app.get("/profile/avdhesh", function (req, res) {
-//   res.send("This is great profile build by hard-work");
-// });
+app.get("/user/:username", function (req, res) {
+  res.send(`hello from userDatabase - ${req.params.username}`);
+});
+
 app.get("/about", function (req, res) {
   res.send("About Page!");
 });
@@ -41,4 +34,4 @@ app.get("/root", function (req, res) {
   res.send("root page is here!! !");
 });
 
-app.listen(4000);
+app.listen(3000);
