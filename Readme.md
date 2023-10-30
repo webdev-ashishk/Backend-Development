bfc7921fc39b197228078be92661c15ba3326abb
webdev-ashishk <webdev.ashishk@gmail.com>
2023-10-30 08:47:27 +0530

    Day#1 and Day2 summery

--- Readme.md
+++
@@ -1,7 +0,0 @@
-# Day-1
-
-> 1. import and export
-> 2. routes
-> 3. middlewares
-
-# Day-2
--- server.js
+++ server.js
@@ -1,53 +1,20 @@
+const { log } = require("console");
 const express = require("express");
 const app = express();
-//configure ejs
-app.set("view engine", "ejs");
-//configure static files
-app.use(express.static("./public"));
-//this is middlewares
-app.use(function (req, res, ashish) {
-  // console.log(req);
-  console.log("middleware-1111 ");
-  ashish();
-});
-app.use(function (req, res, anju) {
-  console.log("middleware-222");
-  anju();
-});
-app.use(function (req, res, next) {
-  console.log(" middleware-333");
+const port = 3000;
+//middlewares
+app.use((req, res, next) => {
+  console.log("middleware-1");
   next();
 });
-// routes
-app.get("/", function (req, res) {
-  res.send("Welcome to Backend development by YT by Indian creater_ !");
-});
-app.get("/contact", function (req, res) {
-  res.render("contact");
-});
-
-//Dynamic routes
-app.get("/profile/:username", function (req, res) {
-  res.send(`hello from profile ${req.params.username}`);
-});
-app.get("/user/:username", function (req, res) {
-  res.send(`hello from userDatabase - ${req.params.username}`);
-});
-//Template engine
-app.get("/template", function (req, res) {
-  res.render("index");
+//ejs setup
+app.set("view engine", "ejs");
+// static file setup
+app.use(express.static("./public"));
+app.get("/", (req, res) => {
+  res.render("index", { age: 20 });
 });
 
-//error handling
-app.get("/error", function (req, res, next) {
-  throw Error("Something Went wrong 404");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
-app.use(function errorHandler(err, req, res, next) {
-  if (res.headersSent) {
-    return next(err);
-  }
-  res.status(500);
-  res.render("error", { error: err });
-});
-
-app.listen(3000);
