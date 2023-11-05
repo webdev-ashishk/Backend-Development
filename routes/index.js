013eb8d310f9307cc98a2ec850616d31202e65b1
webdev-ashishk <webdev.ashishk@gmail.com>
2023-11-05 20:44:54 +0530

    only render login logout page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.render("index");
 });
+/* Get Form-page. */
 router.post("/form", async (req, res) => {
   console.log(req.body);
   const obj = {
@@ -17,4 +18,24 @@ router.post("/form", async (req, res) => {
   res.json({ success: true });
 });
 
+// day#6 creating login logout feature!
+
+/* GET -page. */
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+/* .post used to send the data || create the data */
+router.post("/register", (req, res) => {
+  res.render("register");
+});
+router.post("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/logout", (req, res) => {
+  res.redirect("/");
+});
 module.exports = router;
