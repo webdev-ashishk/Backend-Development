16fb63ffeee89b50b1a6e6e349f8546243a6417f
webdev-ashishk <webdev.ashishk@gmail.com>
2023-11-06 18:58:36 +0530

    login logout in progress

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ const RegisterModel = require("../models/register.models.js");
 router.get("/", function (req, res) {
   const token = req.cookies.token;
   console.log("token : " + token);
+  if (token) {
+    res.render("logout");
+  } else {
+    res.render("login");
+  }
   res.render("index");
 });
 /* Get Form-page. */
@@ -30,7 +35,14 @@ router.get("/register", (req, res) => {
 router.get("/login", (req, res) => {
   res.render("login");
 });
-
+router.get("/logout", (req, res) => {
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.render("logout");
+  // res.send("Welcome to logout page @#");
+});
 /* .post used to send the data || create the data */
 router.post("/register", async (req, res) => {
   console.log(req.body);
@@ -39,13 +51,18 @@ router.post("/register", async (req, res) => {
     gmail: req.body.gmail,
     password: req.body.password,
   };
-  await RegisterModel.create(obj);
-  // res.send("register is completed");
+  const _gmail = await RegisterModel.findOne({ gmail: obj.gmail });
+  if (_gmail) {
+    return res.redirect("/login");
+  } else {
+    await RegisterModel.create(obj);
+  }
+
   res.cookie("token", "Singh", {
     expires: new Date(Date.now() + 900000),
     httpOnly: true,
   });
-  res.redirect("/login");
+  res.redirect("/logout");
 });
 router.post("/login", async (req, res) => {
   console.log(req.body);
@@ -57,15 +74,22 @@ router.post("/login", async (req, res) => {
   // if (userGmail) return res.redirect("/login");
   if (!userGmail) return res.redirect("/register");
   const isMatch = password == userGmail.password;
-  console.log("userGmail password is " + userGmail.password);
-  console.log("user typed password" + password);
-  console.log("isMatch: " + isMatch);
-  if (!isMatch) return res.redirect("/login");
+  console.log("userGmail password is:: " + userGmail.password);
+  console.log("user typed password is:: " + password);
+  console.log("isMatch:: " + isMatch);
   if (isMatch) return res.redirect("/logout");
+  if (!isMatch) return res.redirect("/login");
+
+  res.cookie("token", "Singh", {
+    expires: new Date(Date.now() + 900000),
+    httpOnly: true,
+  });
 });
 router.get("/logout", (req, res) => {
-  // res.redirect("/");
-
-  res.render("logout");
+  res.cookie("token", null, {
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  });
+  res.redirect("/logout");
 });
 module.exports = router;
